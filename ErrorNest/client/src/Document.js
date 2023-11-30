630f1ee5493066d2d02f0b1f4d2b00f3a2e575b1
alkjf <alkjfgh@naver.com>
2023-11-30 18:14:26 +0900

    documnet

--- ErrorNest/client/src/Document.js
+++ ErrorNest/client/src/Document.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react'
 import { useLocation } from 'react-router-dom'
-import { Link } from 'react-scroll'
 import axios from 'axios'
 
 import './css/document.scss'
@@ -40,13 +39,37 @@ function Document(props) {
         })
     }
 
+    function scrollToElement(id) {
+        const element = document.getElementById(id);
+        if (element) element.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // function initIndexHtml() {
+    //     return indexList.map((index, i) => (
+    //             <span key={i}>
+    //                 <a href={"#s-"+index.aText.substring(0,index.aText.length)}>{index.aText}</a>
+    //                 {index.spanText}
+    //             </span>
+    //     ));
+    // }
+
+    function renderIndexItem(index) {
+        return (
+            <span>
+      <a href={"#s-" + index.aText.substring(0, index.aText.length)}>{index.aText}</a>
+                {index.spanText && (
+                    <div>
+                        <span>{index.spanText}</span>
+                        {index.subIndex && renderIndexItem(index.subIndex)}
+                    </div>
+                )}
+    </span>
+        );
+    }
+
     function initIndexHtml() {
-        console.log("s-"+indexList[0].aText.substring(0,1))
         return indexList.map((index, i) => (
-            <span key={i}>
-                <Link to={"s-"+index.aText.substring(0,1)} spy={true} smooth={true} offset={-70} duration={500}>{index.aText}</Link>
-                {index.spanText}
-            </span>
+            <div key={i}>{renderIndexItem(index)}</div>
         ));
     }
 
@@ -55,10 +78,6 @@ function Document(props) {
 
         const content = res.data.content
         const renderedContents = extractElements(content) // JSX 로 변환하여 렌더링
-
-        renderedContents.map((content) => {
-            console.log(content)
-        })
 
         const indexHtml = initIndexHtml()
 
@@ -69,10 +88,12 @@ function Document(props) {
 
     useEffect(() => {
         const this_url = location.pathname
+        const hash = location.hash ? location.hash : undefined
 
         setIndexList([])
 
         urlDatatest(this_url).then(r => {
+            if(hash) scrollToElement(hash.substring(1, hash.length))
         })
 
     }, [ location ])
@@ -82,7 +103,8 @@ function Document(props) {
             <div className="container">
                 <article>
                     <h2>{title}</h2>
-                    <div className="index-list">{renderedIndex}</div>
+                    <div className="index-list" id="top">{renderedIndex}</div>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
                     {renderedContents}
                 </article>
                 <aside>
