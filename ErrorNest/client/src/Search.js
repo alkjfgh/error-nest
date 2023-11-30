2c387a8edc57a37cfc64be4d03907ccec4505cba
Jun983 <epcnd333@naver.com>
2023-11-30 22:37:13 +0900

    231130 logo img Link 수정

--- ErrorNest/client/src/Search.js
+++ ErrorNest/client/src/Search.js
@@ -1,14 +1,21 @@
 
 import { useState, useEffect } from "react";
-
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Search = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get('q');
+    const [document, setDocument] = useState({});
 
-
+    console.log(`location >> ${location}`);
+    console.log(`location.search >> ${location.search}`);
 
     return (
         <div>
             <h2>Search Page Success !!!</h2>
+            <p>Search Result >> {query}</p>
         </div>
     )
 }
--- ErrorNest/server/controller/editController.js
+++ ErrorNest/server/controller/editController.js
@@ -1,16 +1,16 @@
-const Edit = require("../db/schema/edit"); // Get edit schema
+// const Edit = require("../db/schema/edit"); // Get edit schema
 const logger = require("../log/logger");
 
 /** edit CRUD */
 const editCRUD = async (req, res, next) => {
-    try {
-        //sample code
-        const edits = await Edit.find({}); // 몽고디비의 db.users.find({}) 쿼리와 같음
-        res.json({edits});
-    } catch (err) {
-        logger.error(err);
-        next(err);
-    }
+    // try {
+    //     //sample code
+    //     const edits = await Edit.find({}); // 몽고디비의 db.users.find({}) 쿼리와 같음
+    //     res.json({edits});
+    // } catch (err) {
+    //     logger.error(err);
+    //     next(err);
+    // }
 }
 
 /** Exports CRUD functions */
