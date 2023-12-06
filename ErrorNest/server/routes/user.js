eca0ecdb0223c437823ac36f2a21b560fdf1b67f
Jun983 <epcnd333@naver.com>
2023-12-06 20:20:46 +0900

    document

--- ErrorNest/server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require('../controller/userController');
-const router = express.Router();
-
-/** GET /user controller로 분리 */
-router.get('/', userController.userFindAll);
-
-module.exports = router;
