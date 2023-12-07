1dd43cab7f34380b380fcebbfe8a4a0cc4cdf51a
Jun983 <epcnd333@naver.com>
2023-12-07 20:58:56 +0900

    reportHistory
    controller & router 파일 생성

--- ErrorNest/client/src/ReportHistory.js
+++ ErrorNest/client/src/ReportHistory.js
@@ -21,36 +21,30 @@ const ReportHistory = () => {
     /** 로그인 한 계정의 레벨 가져오기 (user or admin) */
     const getMemberLevel = async (thisUri) => {
         const writer = cookies.userid;
-        const response = await axios.get(`${thisUri}/getMember?id=${writer}`);
+        const writerName = cookies.username;
+        const response = await axios.get(`${thisUri}/getMember?id=${writer}&username=${writerName}`);
+        console.log(response);
         setLevel(response.data.level);
-        console.log(`level >> ${level}`);
+        setWriter(response.data.id);
     }
 
     /** 로그인 했을 시 id, 안했을 시 ip로 writer 설정 */
     const getWriter = async () => {
-        console.log(`cookies.userid >> ${cookies.userid}`);
-        setLevel("noLevel");
-
-        if(cookies.userid !== undefined)
+        if(cookies.userid !== undefined) {
+            console.log(`cookies.userid >> ${cookies.userid}`);
             setWriter(cookies.userid);
-            return cookies.userid;
+        } else {
+            const response = await fetch("https://api64.ipify.org?format=json");
+            const data = await response.json();
 
-        const response = await fetch("https://api64.ipify.org?format=json");
-        const data = await response.json();
-
-        console.log(`data.ip >> ${data.ip}`);
-        setWriter(data.ip);
-        return data.ip;
+            console.log(`data.ip >> ${data.ip}`);
+            setWriter(data.ip);
+        }
     }
 
     useEffect(() => {
         const thisUri = location.pathname;
-
-        getWriter().then(result => setWriter(result));
-        console.log(`writer >> ${writer}`);
-
-        if (cookies.userid !== undefined)
-            getMemberLevel(thisUri);
+        getWriter().then(() => getMemberLevel(thisUri));
     }, []);
 
 
@@ -65,6 +59,9 @@ const ReportHistory = () => {
     return (
         <div>
             <h2>신고 목록</h2>
+            writer: {writer}<br />
+            writerName: {cookies.username}<br />
+            level: {level}<br />
             <ul>
                 {posts.map(post => (
                     <li key={post.id}>{post.title}</li>
--- ErrorNest/server/controller/reportHistoryController.js
+++ ErrorNest/server/controller/reportHistoryController.js
@@ -1,24 +1,30 @@
 const member = require("../db/schema/member");
 const logger = require("../log/logger");
+const CryptoJS = require("crypto-js");
+
+
+function decryptCookie(cipherText, key) {
+    const bytes  = CryptoJS.AES.decrypt(cipherText, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
 
 /** reportHistory CRUD */
-
 const memberSelect = async (req, res, next) => {
     console.log(`memberSelect controller 입장 성공 !!`);
 
+    const memberId = decryptCookie(req.query.id, req.query.username); // 복호화
+    console.log(`id >> ${req.query.id}`);
+    console.log(`username >> ${req.query.username}`);
+    console.log(`memberId >> ${memberId}`);
     try {
-        const memberId = req.query;
-        console.log(memberId);
-
-        const result = await member.findOne(memberId);
-        console.log(result);
+        const result = await member.findOne({id: memberId});
         console.log(`level >> ${result.level}`);
 
-        res.json({success: true, level: result.level, message: "맴버 가져오기 성공"});
+        res.json({success: true, id: memberId, level: result.level, message: "맴버 가져오기 성공"});
     } catch (err) {
         logger.error(err);
         next(err);
-        res.json({success: false, level: "noLogin", message: "맴버 가져오기 실패"});
+        res.json({success: false, id: memberId, level: "noLogin", message: "맴버 가져오기 실패"});
     }
 }
 const reportSelect = async (req, res, next) => {
