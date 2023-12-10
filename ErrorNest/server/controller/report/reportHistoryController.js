93cf822c974cb31b77900cc45a5b761dc6b33677
Jun983 <epcnd333@naver.com>
2023-12-10 20:29:45 +0900

    reportHistoryController
    관리자일 때 모든 신고 내용이 보이게 함

--- ErrorNest/server/controller/report/reportHistoryController.js
+++ ErrorNest/server/controller/report/reportHistoryController.js
@@ -1,40 +1,38 @@
 const Report = require("../../db/schema/report/report");
+const Member = require("../../db/schema/member/member");
 const logger = require("../../log/logger");
 const {encryptCookie, decryptCookie} = require('../encript/encriptCookie');
 
 /** reportHistory CRUD */
 const reportSelectAll = async (req, res, next) => {
     console.log(`reportSelect controller 입장 성공 !!\n\n`);
-    const userInfo = req.body;
-    console.log(userInfo);
+    const reqData = req.body;
+    console.log(reqData);
 
-    if (userInfo.isLogin) {
+    if (reqData.isLogin) {
         const member = {
-            id: userInfo.userid,
-            str_id: userInfo.userkey
+            id: reqData.userid,
+            str_id: reqData.userkey
         }
-        userInfo.userid = decryptCookie(member);
+        reqData.userid = decryptCookie(member);
     }
 
-    console.log("--- decryption result ---");
-    console.log(userInfo);
-
     try {
+        const userInfo = await Member.findOne({id: reqData.userid});
+        console.log("---- userInfo ----");
+        console.log(userInfo);
         let result = [];
 
-        if (userInfo.username === "관리자")
+        if (userInfo.level === "admin")
             result = await Report.find({});
         else
-            result = await Report.find({writer: userInfo.userid});
+            result = await Report.find({writer: reqData.userid});
 
-        console.log("---- result ----");
-        console.log(result);
-
-        res.json({success: true, result: result, writer: userInfo.userid, message: "게시판 리스트 가져오기 성공"});
+        res.json({success: true, result: result, writer: reqData.userid, message: "게시판 리스트 가져오기 성공"});
     } catch (err) {
         logger.error(err);
         next(err);
-        res.json({success: false, result: null, writer: userInfo.userid, message: "게시판 리스트 가져오기 실패"});
+        res.json({success: false, result: null, writer: reqData.userid, message: "게시판 리스트 가져오기 실패"});
     }
 }
 
