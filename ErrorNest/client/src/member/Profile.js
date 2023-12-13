251069a0d370eb5cdec050eaa7f41259fe62c0a9
Spica-blue <heo1356@gmail.com>
2023-12-13 13:13:49 +0900

    프로필 하는중

--- ErrorNest/client/src/member/Profile.js
+++ ErrorNest/client/src/member/Profile.js
@@ -57,6 +57,7 @@ const Profile = (props) => {
         return false
     }
 
+
     const getProfile = async () => {
         const target = await getTarget()
         const isEqual = getUser(target)
