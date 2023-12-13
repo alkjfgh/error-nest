e09d3a39a2794db26f96588d529f58f49643e323
choieunhee <zxcvbnm4830@naver.com>
2023-12-13 13:33:17 +0900

    프로필 수정

--- ErrorNest/client/src/member/Profile.js
+++ ErrorNest/client/src/member/Profile.js
@@ -135,6 +135,7 @@ const Profile = (props) => {
                         </form>
                     </div>
                 }
+
             </div>
             {writtenList.length > 0 &&
                 <div>
