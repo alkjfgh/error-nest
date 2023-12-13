c164ffd4db41ccd12960ada9379d4f346ac2b248
Jun983 <epcnd333@naver.com>
2023-12-13 15:00:39 +0900

    Report css 재구성

--- ErrorNest/server/routes/report/report.js
+++ ErrorNest/server/routes/report/report.js
@@ -5,9 +5,13 @@ const router = express.Router();
 /** GET /report */
 // router.get('/*', reportController.documentSelect);
 router.post('/getDocument/', reportController.documentSelect);
+
 router.post('/select/', reportController.reportSelect);
+
 router.post('/insert/', reportController.reportInsert);
+
 router.put('/update/', reportController.reportUpdate);
+
 router.put('/updateCancel/', reportController.reportUpdateCancel);
 
 
