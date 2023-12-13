f2ad8cfdd663a2e5a19ac835a7025eecb573e511
alkjf <alkjfgh@naver.com>
2023-12-13 22:51:17 +0900

    multer3

--- ErrorNest/server/controller/document/uploadController.js
+++ ErrorNest/server/controller/document/uploadController.js
@@ -4,13 +4,15 @@ const logger = require('../../log/logger')
 const fileUpload = async (req, res, next) => {
     console.log('fileUpload')
     try {
+        console.log(req.file.location)
         const fileData = req.file;
         const fileName = fileData.originalname;
         const mimeType = fileData.mimetype;
         const size = fileData.size;
         const file = {
             fileName: req.body.fileName,
-            filePath: './uploads/'+req.body.category+'/'+req.file.originalname,
+            // filePath: './uploads/'+req.body.category+'/'+req.file.originalname,
+            filePath: req.file.location,
             fileDes: req.body.fileDes,
             category: req.body.category
         }
--- ErrorNest/server/routes/document/upload.js
+++ ErrorNest/server/routes/document/upload.js
@@ -2,8 +2,27 @@ const express = require('express');
 const uploadController = require('../../controller/document/uploadController');
 const router = express.Router();
 const multer = require('multer');
+const AWS = require("aws-sdk");
+const multerS3 = require('multer-s3');
 const path = require('path');
 const fs = require('fs');
+
+const s3 = new AWS.S3({ accessKeyId:"epcnd333@naver.com", secretAccessKey:"skssks33??!!" });
+//* aws region 및 자격증명 설정
+// AWS.config.update({
+//     accessKeyId: "epcnd333@naver.com",
+//     secretAccessKey: "skssks33??!!",
+//     region: 'ap-northeast-2',
+// });
+
+// Multer를 위한 필터. 이미지 파일만 Multer를 통해 업로드하고 아닌 경우 에러 발생.
+const multerfilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')){
+        cb(null, true);
+    } else {
+        cb(console.log('Not image file upload tried'), false);
+    }
+}
 
 // 이미지를 저장할 경로와 파일명을 설정합니다.
 const storage = multer.diskStorage({
@@ -25,9 +44,23 @@ const storage = multer.diskStorage({
     }
 })
 
-//한글 깨짐
+// multer 저장소 및 필터 설정
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'error-nest',
+        contentType: multerS3.AUTO_CONTENT_TYPE, // content type 들어오는대로 설정
+        key: function(req, file, cb){
+            if(file.originalname === undefined || file.originalname === null){ }
+            else {
+                cb(null, Date.now() + '.' + file.originalname.split('.').pop()); // 파일 이름 설정을 위한 callback function
+            }
+        }
+    }),
+    fileFilter: multerfilter,
+},'NONE');
 
-const upload = multer({ storage: storage })
+// const upload = multer({storage: storage })
 
 router.post('/', upload.single('file'), uploadController.fileUpload);
 
